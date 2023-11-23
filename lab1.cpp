65e749dbb135aeebdbfd5e28593a67d71caf4a4a
Kobozev Alexandr <kobozevsasha223@gmail.com>
2023-11-23 18:48:55 +0300

    rewrite code

--- lab1.cpp
+++ lab1.cpp
@@ -7,42 +7,34 @@ int main(int argc, char *argv[]){
 	double start, finish;
 	start = omp_get_wtime();
 	
-	const int count = 1000000;
+	const int count = 2000000000;
 	const int threads = 16;
 	const int random_seed = 567;
-	const int reps = 2000;
-	int** array = 0;
+	int* array = 0;
 	int max = -1;
 	
 	srand(random_seed);
 	
 	printf("OpenMP: %d;\n", _OPENMP);
 	
-	array = (int**)malloc(reps * sizeof(int*));
-	for (int i = 0; i < reps; ++i) {
-		array[i] = (int*)malloc(count * sizeof(int));
-		for (int j = 0; j < count; ++j) {
-			array[i][j] = rand();
-		}
+	array = (int*)malloc(count * sizeof(int));
+	for (int i = 0; i < count; ++i) {
+			array[i]= rand();
 	}
 	
 	for (int th = 1; th < threads; ++th){
-		for (int i = 0; i < reps; ++i){
-			max = -1;
-			#pragma omp parallel num_threads(th) shared(array, i, count) reduction(max: max) default(none)
-			{
-				#pragma omp for
-				for (int j = 0; j < count; ++j) {
-					if (array[i][j] > max) {
-						max = array[i][j];
-					}
+		max = -1;
+		#pragma omp parallel num_threads(th) shared(array, count) reduction(max: max) default(none)
+		{
+			#pragma omp for
+			for (int i = 0; i < count; ++i) {
+				if (array[i] > max) {
+					max = array[i];
 				}
 			}
 		}
 	}
-	for (int i = 0; i < reps; ++i) {
-		free(array[i]);
-	}
+	
 	free(array);
 	finish = omp_get_wtime();
 	printf("Time: %lf \n", (finish - start));
