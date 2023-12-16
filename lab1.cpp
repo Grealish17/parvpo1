c35b1897653f817debce64d133e80b3f88e8afe9
Kobozev Alexandr <kobozevsasha223@gmail.com>
2023-12-16 16:12:05 +0300

    add reps

--- lab1.cpp
+++ lab1.cpp
@@ -3,8 +3,9 @@
 #include <ctime>
 
 int main(int argc, char *argv[]){
-	const int count = 2000000000;
+	const int count = 20000000;
 	const int threads = 16;
+    const int reps = 200;
 	const int random_seed = 567;
 	int* array = 0;
 	int max = -1;
@@ -12,27 +13,29 @@ int main(int argc, char *argv[]){
 	srand(random_seed);
 	
 	printf("OpenMP: %d;\n", _OPENMP);
-	
+
+    double start, finish;
+    start = omp_get_wtime();
+
 	array = (int*)malloc(count * sizeof(int));
 	for (int i = 0; i < count; ++i) {
 		array[i]= rand();
 	}
-	
-	double start, finish;
-	start = omp_get_wtime();
-	
-	for (int th = 1; th < threads; ++th){
-		max = -1;
-		#pragma omp parallel num_threads(th) reduction(max: max)
-		{
-			#pragma omp for
-			for (int i = 0; i < count; ++i) {
-				if (array[i] > max) {
-					max = array[i];
-				}
-			}
-		}
-	}
+
+	for(int j(0); j<reps; ++j){
+        for (int th = 1; th < threads; ++th){
+            max = -1;
+            #pragma omp parallel num_threads(th) reduction(max: max)
+            {
+                #pragma omp for
+                for (int i = 0; i < count; ++i) {
+                    if (array[i] > max) {
+                        max = array[i];
+                    }
+                }
+            }
+        }
+    }
 	
 	free(array);
 	finish = omp_get_wtime();
